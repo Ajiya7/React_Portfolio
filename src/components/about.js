6d7123a4593f1749dc780aec9c6a674ed1d6ecad
Faruk Ajiya <Ajiyafaruk@gmail.com>
2023-03-05 15:03:27 -0800

    css completed and images uploaded

--- src/components/about.js
+++ src/components/about.js
@@ -1,19 +1,58 @@
 import React from "react";
 import "../";
-import { Col } from 'react-bootstrap';
+import { Col } from "react-bootstrap";
 
 export default function About() {
   return (
     <section className="title">
       <div className="row justify-content-center" id="about-container">
         <Col lg={6} md={12}>
+          <br />
+          <br />
           <center>
-          <h1 class="name">Faruk Ajiya</h1>
-          <h2 class="job">Full stack web developer</h2>
+            <h1 class="name">Faruk Ajiya</h1>
+            <h2 class="job">Full-Stack Web Developer</h2>
           </center>
+          <br />
+          <br />
+          <br />
           <p>
-            <h4>Hi i'm Faruk.</h4>
+            <center>
+              {" "}
+              <h4>Hi i'm Faruk.I'm a Full-Stack Web Developer</h4>
+            </center>
           </p>
+          <br />
+          <br />
+          <br />
+          <center>
+            <h2>Skills</h2>
+          </center>
+          <br />
+          <br />
+          <div class="skills">
+            <center>
+            <div class="skills_skill">HTML</div>
+            <div class="skills_skill">CSS</div>
+            <div class="skills_skill">GIT</div>
+            <div class="skills_skill">Github</div>
+            </center>
+            <center>
+            <div class="skills_skill">Responsive Design</div>
+            <div class="skills_skill">Terminal</div>
+            <div class="skills_skill">Javascript</div>
+            <div class="skills_skill">React</div>
+            </center>
+            <center>
+            <div class="skills_skill">Node.js</div>
+            <div class="skills_skill">JSON</div>
+            <div class="skills_skill">Express</div>
+            <div class="skills_skill">Mysql</div>
+            </center>
+            <center>
+            <div class="skills_skill">MongoDB</div>
+            </center>
+          </div>
         </Col>
       </div>
     </section>
--- src/components/project.js
+++ src/components/project.js
@@ -1,58 +1,66 @@
-import React from 'react'; 
-import projects from '../projects.json'
+import React from "react";
+import projects from "../projects.json";
+import image2 from "../assets/images/vapour.png";
+import image1 from "../assets/images/movielookup.png"
+const imagesarray = [image1,image2] 
 
 function ProjectCard(props) {
-    return (
-      <div className="card"> 
-        <div className="img-container">
-          <img alt={props.name} src={props.image}/>
-        </div>
-        <div className="content">
-          <ul>
-            <li>
-              <p className="project-title">{props.name}</p>
-            </li>
-            <li>
-            <p className="project-description">{props.description}</p>
-            </li>
-            <li>
-              <div className="project-link">
-              <a href={props.deployedapp}>Link to application</a> 
-              </div>
-            </li>
-          </ul>
-        </div>
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
       </div>
-    );
+      <div className="content">
+        <ul>
+          <li>
+            <p className="project-title">{props.name}</p>
+          </li>
+          <li>
+            <p className="project-description">{props.description}</p>
+          </li>
+          <li>
+            <div className="project-link">
+              <a href={props.deployedapp}>Link to application</a>
+              <br />
+              <a href={props.githublink}>Link to code</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 function Wrapper(props) {
-    return <div className="wrapper">{props.children}</div>;
+  return <div className="wrapper">{props.children}</div>;
 }
 
-function Projects () {  
-    return (
+function Projects() {
+  return (
     <>
-    <div className="project">
-    <h1 className="title">Projects</h1>
-    </div>
+      <div class="project">
+        <div class="heading_section_title_container">
+          <h2 class="heading_section_title heading_section_title_C">
+            Portfolio
+          </h2>
+        </div>
 
-      <Wrapper>
-        {projects.map((project) => (
-          <ProjectCard 
-          name={project.name}
-          key={project.id}
-          image={project.image}
-          github={project.github}
-          deployedapp={project.deployedapp}
-          skills={project.skills}
-          description={project.description}
-          />
-        ))}
-      </Wrapper>
+        <Wrapper>
+          {projects.map((project,i) => (
+            <ProjectCard
+              name={project.name}
+              key={project.id}
+              github={project.github}
+              deployedapp={project.deployedapp}
+              description={project.description}
+              githublink={project.githublink}
+              image = {imagesarray[i]}
+            />
+          ))}
+        </Wrapper>
+      </div>
     </>
-    
-    );
-  }
-  
-  export default Projects;
+  );
+}
+
+export default Projects;
